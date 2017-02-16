3071dedb8664e1b80fe227edb437b0bbebe99a76
Alexander Chepurnoy <chepurnoy.aleksandr@gmail.com>
2017-02-16 17:26:53 +0200

    First commit.

---
+++ addon/mixins/adapter.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  storeEvents: Ember.inject.service(),
+
+  createRecord(store, type, snapshot) {
+    return this._super(...arguments).then((result) => {
+      this.get('storeEvents').trigger('create-record', snapshot.record, result.id);
+      return result;
+    });
+  },
+
+  deleteRecord(store, type, snapshot) {
+    return this._super(...arguments).then((result) => {
+      this.get('storeEvents').trigger('delete-record', snapshot.record, snapshot.id);
+      return result;
+    });
+  },
+
+  updateRecord(store, type, snapshot) {
+    return this._super(...arguments).then((result) => {
+      this.get('storeEvents').trigger('update-record', snapshot.record, snapshot.id);
+      return result;
+    });
+  },
+});
---
+++ app/services/store-events.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-store-events/services/store-events';
